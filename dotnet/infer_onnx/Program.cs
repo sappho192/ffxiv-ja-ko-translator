1acc35fe8d674d75b9246f310aabed3900370d5c
TAE-IN Sappho KIM <sappho192@gmail.com>
2024-01-11 17:25:33 +0900

    Add BertJapanese tokenizer test code in dotnet

--- dotnet/infer_onnx/Program.cs
+++ dotnet/infer_onnx/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FastBertTokenizer;
+
 Console.WriteLine("Hello, World!");
+
+TestBertJapanese();
+
+void TestBertJapanese()
+{
+    var tok = new BertTokenizer();
+    //tok.LoadFromHuggingFaceAsync("bert-base-uncased").GetAwaiter().GetResult();
+    var vocab = File.OpenText("vocab.txt");
+    tok.LoadVocabulary(vocab, false);
+    //var tokenizerJson = File.OpenText("D:\\DATA\\tokenizer.json");
+    //tok.LoadTokenizerJson(tokenizerJson.BaseStream);
+    // Full-width symbols should be converted to half-width symbols
+    // Example: ！ -> !
+    // List of full-width symbols are: https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms
+    var (inputIds, attentionMask, tokenTypeIds) = tok.Encode("こんにちは!");
+    Console.WriteLine(string.Join(", ", inputIds.ToArray()));
+    var decoded = tok.Decode(inputIds.Span);
+    Console.WriteLine(decoded);
+}
