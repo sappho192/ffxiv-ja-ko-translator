b03fc2ec1c6435d571558a494403d3f7b48d459e
TAE-IN Sappho KIM <sappho192@gmail.com>
2024-01-24 09:40:10 +0900

    Move everything into onnx_project folder

---
+++ onnx_project/dotnet/infer_onnx/Program.cs
@@ -0,0 +1,17 @@
+﻿using infer_onnx;
+
+TestFunctions tf = new();
+
+Console.WriteLine("Testing tokenizer...\n");
+tf.TestBertJapanese("打ち合わせが終わった後にご飯を食べましょう。");
+
+Console.WriteLine();
+tf.TestKoGPT2();
+
+Console.WriteLine("Done tokenizer tests.");
+Console.WriteLine("Testing ONNX...\n");
+
+tf.TestONNX();
+
+
+
